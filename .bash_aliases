c560e7c91b178bd112bec888e23a793a49fd4927
Mastize <mastizenone@gmail.com>
2020-10-28 19:55:40 +0800

    init commit

---
+++ .bash_aliases
@@ -0,0 +1,12 @@
+# common  
+alias ll='ls -lFh'
+alias la='ls -AlFh'
+alias l='ls -F'
+alias em='TERM=xterm-24bits emacs -nw'
+alias mk='make -j32'
+
+# git
+alias gs='git status'
+alias gl='git log'
+
+alias ct='ctags -R -e --exclude=build .'
---
+++ .custom.el
@@ -0,0 +1,11 @@
+;; personal .custom.el based on github.com/redguardtoo/emacs.d
+
+;; set indent of c/c++ as 2 spaces
+(setq-default c-basic-offset 2)
+
+;; stop emacs from redrawing when recenter
+(setq recenter-redisplay nil)
+
+;; my fav theme )
+(load-theme 'sanityinc-tomorrow-eighties t)
+
