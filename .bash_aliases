d14a733a39388402ae1c5eb696dbf1e79aab484d
Mastize <mastizenone@gmail.com>
2020-11-02 20:58:39 +0800

    update

--- .bash_aliases
+++ .bash_aliases
@@ -1,7 +1,7 @@
 # common  
+alias l='ls -F'
 alias ll='ls -lFh'
 alias la='ls -AlFh'
-alias l='ls -F'
 alias em='TERM=xterm-24bits emacs -nw'
 alias mk='make -j32'
 
@@ -9,4 +9,5 @@ alias mk='make -j32'
 alias gs='git status'
 alias gl='git log'
 
+# project
 alias ct='ctags -R -e --exclude=build .'
--- .custom.el
+++ .custom.el
@@ -1,11 +1,20 @@
 ;; personal .custom.el based on github.com/redguardtoo/emacs.d
 
+(setq compile-command "make")
+
 ;; set indent of c/c++ as 2 spaces
 (setq-default c-basic-offset 2)
 
 ;; stop emacs from redrawing when recenter
 (setq recenter-redisplay nil)
 
-;; my fav theme )
+;; my fav theme :)
 (load-theme 'sanityinc-tomorrow-eighties t)
 
+(defun day-theme ()
+    (interactive)
+    (load-theme 'sanityinc-tomorrow-day t))
+
+(defun night-theme ()
+    (interactive)
+    (load-theme 'sanityinc-tomorrow-eighties t))
